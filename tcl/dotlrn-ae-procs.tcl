2410feff2a78105f1a9fdd677cfa3a82bd82e7e9
Hector Romojaro <hector.romojaro@gmail.com>
2018-03-27 14:22:30 +0000

    Fix typos

--- tcl/dotlrn-ae-procs.tcl
+++ tcl/dotlrn-ae-procs.tcl
@@ -49,7 +49,7 @@ ad_proc -public dotlrn_ae::remove_applet {} {
 ad_proc -public dotlrn_ae::add_applet_to_community {
     community_id
 } {
-    Add the ae applet to a specifc dotlrn community
+    Add the ae applet to a specific dotlrn community
 } {
     set portal_id [dotlrn_community::get_portal_id -community_id $community_id]
 
@@ -77,7 +77,7 @@ ad_proc -public dotlrn_ae::remove_applet_from_community {
 } {
     remove the applet from the community
 } {
-    ad_return_complaint 1 "[applet_key] remove_applet_from_community not implimented!"
+    ad_return_complaint 1 "[applet_key] remove_applet_from_community not implemented!"
 }
 
 ad_proc -public dotlrn_ae::add_user {
@@ -92,14 +92,14 @@ ad_proc -public dotlrn_ae::remove_user {
     user_id
 } {
 } {
-    ad_return_complaint 1 "[applet_key] remove_user not implimented!"
+    ad_return_complaint 1 "[applet_key] remove_user not implemented!"
 }
 
 ad_proc -public dotlrn_ae::add_user_to_community {
     community_id
     user_id
 } {
-    Add a user to a specifc dotlrn community
+    Add a user to a specific dotlrn community
 } {
     set package_id [dotlrn_community::get_applet_package_id -community_id $community_id -applet_key [applet_key]]
     set portal_id [dotlrn::get_portal_id -user_id $user_id]
